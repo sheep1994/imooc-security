6c0c92a1edddda2f8ef48fdae26511da0d191225
郭兵 <m13571829170@163.com>
2019-01-28 17:17:36 +0800

    添加用户实体类字段代码提交

--- imooc-security-demo/src/main/java/com/talent/model/User.java
+++ imooc-security-demo/src/main/java/com/talent/model/User.java
@@ -2,6 +2,8 @@ package com.talent.model;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.Date;
+
 /**
  * @author guobing
  * @Title: User
@@ -16,6 +18,8 @@ public class User {
     private String username;
 
     private String password;
+
+    private Date birthday;
 
     public int getId() {
         return id;
@@ -39,6 +43,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 
     @Override
